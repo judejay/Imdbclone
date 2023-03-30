2d7c44a8b147ab44d29e046e63c7a36514fd5289
judejay <judejay26@gmail.com>
2023-03-30 10:58:43 +0100

    movie page

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -30,7 +30,11 @@ type ResultsProps ={
 const Results = ({results}: ResultsProps) => {
   return (
     <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">{results.map(movie =>{
-        return <Card movie={movie}></Card>
+        return(
+          <div key={movie.id} >
+         
+          <Card movie={movie}></Card> </div>
+        ) 
     } 
         )}</div>
   )
